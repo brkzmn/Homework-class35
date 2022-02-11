4cd9ce13bb5fd745e0952ad57fd09bc0273b7cae
Burak Özmen <brkzmn.wd@gmail.com>
2022-02-11 14:23:39 +0100

    ex1-done

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -18,28 +18,35 @@ Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-Usin
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
 function requestData(url) {
-  // TODO return a promise using `fetch()`
+  return fetch(url)
+  .then(data => data.json())
+  .catch((error) => {
+    console.log(error);
+  })
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
+  const imageElement = document.createElement("img");
+  imageElement.src = data.img;
+  imageElement.setAttribute("alt", data.alt);
+  document.body.appendChild(imageElement);
   console.log(data);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
+  const errorElement = document.createElement("h1");
+  errorElement.textContent = error;
+  document.body.appendChild(errorElement);
   console.log(error);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+  try {
+    const data = await requestData('https://xkcd.now.sh/?comic=latest')
+    renderImage(data);
+  } catch (error) {
+    renderError(error);
+  }
 }
 
 window.addEventListener('load', main);
